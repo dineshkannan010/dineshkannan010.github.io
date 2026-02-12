e882ae53d2c77bd59e5ffbe9b8317c043acfb458
Dinesh12 <dineshkannan010@gmail.com>
2026-02-12 16:21:53 -0500

    Update build output in docs folder

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'docs',               // <--- Tells Vite to build into the "docs" folder
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
